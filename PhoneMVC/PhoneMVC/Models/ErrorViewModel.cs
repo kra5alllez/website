cce44d8efee6ce6eb62a53cf4ee516b538d18127
kra5alllez <kra5alllez@gmail.com>
2021-06-10 21:59:20 +0300

    add project

---
+++ PhoneMVC/PhoneMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PhoneMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
